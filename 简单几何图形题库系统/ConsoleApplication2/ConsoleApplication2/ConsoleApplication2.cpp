b6582fc1156185f7487a373ac6525b1b921acb63
primehear <762569937@qq.com>
2026-09-17 16:57:46 +0800

    Update ConsoleApplication2.cpp

--- 简单几何图形题库系统/ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
+++ 简单几何图形题库系统/ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -1,9 +1,14 @@
-﻿// ConsoleApplication2.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
-
-#include <iostream>
-
-int main()
+#include<iostream>
+#include<string>
+class TriangleItem
 {
-    std::cout << "Hello World!\n";
-}
+private:
+	float m_area;
+	int m_a;
+	int m_b;
+	int m_c;
+	int m_permeter;
+public:
+	TriangleItem();
+
+};
